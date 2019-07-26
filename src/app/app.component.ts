ab9ac3c847c2fd03f1193c94fbf621a67830c2b0
khuongduy1484 <khuongduy1484@gmail.com>
2019-07-27 04:07:06 +0700

    create layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenStorageService } from './auth/token-storage.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   private roles: string[];
   private authority: string;
-
   constructor(private tokenStorage: TokenStorageService) { }
 
   ngOnInit() {
