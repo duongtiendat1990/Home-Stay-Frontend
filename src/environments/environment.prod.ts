b4da4f8cc1f90e78cfa68a989b1adb9e222b25ab
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-08-19 21:05:47 +0700

    update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDb1ctwB6HB21DVrAPeG8e2_y6BSiKEM1o',
+    authDomain: 'luxurystay-ec208.firebaseapp.com',
+    databaseURL: 'https://luxurystay-ec208.firebaseio.com',
+    projectId: 'luxurystay-ec208',
+    storageBucket: 'luxurystay-ec208.appspot.com',
+    messagingSenderId: '1063558189481',
+    appId: '1:1063558189481:web:e892afe9f7eb18bf'
+  }
 };
